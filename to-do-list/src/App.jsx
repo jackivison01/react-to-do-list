75abb8e1a97c756da9f2a146c571c47eb16abc5f
jackivison01 <jivison2001@gmail.com>
2025-01-05 14:32:29 +0000

    Created basic template components

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -1,3 +1,68 @@
-export default function ToDoList() {
+import { useState } from "react";
 
+function ToDoRow({ toDoText }) {
+  return (
+    <div>
+      <ToDo text={toDoText}></ToDo>
+      <CheckToDo></CheckToDo>
+      <DeleteToDo></DeleteToDo>
+    </div>
+  )
+}
+
+function ToDo({ text }) {
+  return (
+    <text>{text}</text>
+  )
+}
+
+function CheckToDo() {
+  return (
+    <Button value={"Tick"}></Button>
+  )
+}
+
+function DeleteToDo() {
+  return (
+    <Button value={"Bin"}></Button>
+  )
+}
+
+function EntryRow() {
+  return (
+    <div>
+      <ToDoEntry></ToDoEntry>
+      <AddTodoButton></AddTodoButton>
+    </div>
+  )
+}
+
+function ToDoEntry({ }) {
+  return (
+    <input placeholder="Enter todo here:"></input>
+  )
+}
+
+function AddTodoButton() {
+  return (
+    <Button value='+'></Button>
+  )
+}
+
+function Button({ value }) {
+  return (
+    <button>{value}</button>
+  )
+}
+
+export default function ToDoList() {
+  const [todos, setTodos] = useState([])
+
+  return (
+    <>
+      <EntryRow></EntryRow>
+      <ToDoRow toDoText={"Do the laundry"}></ToDoRow>
+      <ToDoRow toDoText={"Make dinner"}></ToDoRow>
+    </>
+  )
 }
